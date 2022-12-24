a5c3aa7bbc1de44e282a09d8e72455a1b531f917
Walter Karabin <walter.karabin@gmail.com>
2022-12-24 17:50:25 -0500

    AutoScroll down

--- santa.js
+++ santa.js
@@ -1,26 +1,29 @@
+// initialize global variables
 var hatPoofDegNum = 15;
 var armDegNum = -135;
-var direction = 0.75;
+var hatDirection = 0.75;
+var armDirection = 0.75;
 var sleighVel = 2; 
+var numDirChanges = 0;
 var mosleighVeleft = true;
 var moveRight = false;
 var sleighX = window.innerWidth;
 var beardTop = 50;
-var beardDir = 2;
 const sleigh = document.getElementById('sleigh');
 const santa = document.getElementById('santa');
+const canv = document.querySelector('#myCanvas1');
 
-
+// functions for Santa's Sleigh
 function santaHat() {
     const hatPoof = document.getElementById('hatPoof');
     
     if(hatPoofDegNum >= 30){
-        direction *= -1;
+        hatDirection *= -1;
     }
     else if(hatPoofDegNum <= 0){
-        direction *= -1;
+        hatDirection *= -1;
     }
-    hatPoofDegNum += direction;
+    hatPoofDegNum += hatDirection;
     hatPoof.style.transform = `rotate(${hatPoofDegNum}deg)`;
     // console.log(degNum);
 
@@ -29,7 +32,14 @@ function santaHat() {
 function santaWave() {
     const arm = document.getElementById('arm');
 
-    armDegNum += direction;
+    if(armDegNum >= -110){
+        armDirection *= -1;
+    }
+    else if(armDegNum <= -160){
+        armDirection *= -1;
+    }
+    armDegNum += armDirection;
+
     arm.style.transform = `rotateZ(${armDegNum}deg)`;
 }
 
@@ -48,6 +58,7 @@ function sleighMove(){
         moveRight = true;
         sleighVel = -2;
         sleigh.style.transform = `rotateY(180deg)`;
+        numDirChanges++;
     }
 
     else if (sleighX < (window.innerWidth + (100)) && moveRight) {
@@ -61,47 +72,67 @@ function sleighMove(){
         moveRight = false;
         sleighVel = 2;
         sleigh.style.transform = `rotateY(0deg)`;
+        numDirChanges++;
     }
 }
 
 function santaLaugh() {
     const beard = document.getElementById('beard');
-
+    var amplitude = 2;
+    const oscillationPeriod = 1;
  
-    beardTop = Math.sin(beardTop);
+    beardTop = amplitude*Math.sin(Date.now() / 100 /oscillationPeriod) + 25;
+    // console.log(beardTop)
 
-    beard.style.top = `${beardTop}`;
+    beard.style.top = `${beardTop}px`;
 }
 
 
-
+// animation control for Santa
 function updateSanta() {
     
-    
+    const period = 1;
     
     var sleighY = 0;
     // console.log(sleigh.style.left);
 
     
     if (sleigh.style.left <= 100) {
-        // rotate and change direction
+        // rotate and change hatDirection
     }
     else if (sleighX > window.innerWidth){
-        // rotate and change direction
+        // rotate and change hatDirection
     }
 
     sleighMove();
     // setInterval(santaLaugh, 500);
-    santaHat();
+    const sineValue = Math.sin(Date.now() / 1000 / period);
+    if (sineValue > 0) {
+        // call the function during positive sine values
+        santaLaugh();
+        // add the hohoho initialization here
+    }
+    else{
+        santaHat();
+    }
     santaWave();
-    requestAnimationFrame(updateSanta);
 
+    if (numDirChanges >= 3) {
+        endAnimation();
+        return;
+    }
+    console.log(numDirChanges);
+    requestAnimationFrame(updateSanta);
 }
 
 requestAnimationFrame(updateSanta);
 
 
 // not related to santa... meant for scroll
+function endAnimation() {
+    canv.scrollIntoView({ behavior: 'smooth' });
+}
+
 window.addEventListener("keypress", function () {
     const div = document.querySelector('#myCanvas1');
 
